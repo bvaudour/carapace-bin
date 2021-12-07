2271935e908f461cd414543bd73558492543ac71
rsteube <rsteube@users.noreply.github.com>
2021-12-07 17:44:16 +0100

    tmp

--- completers/circleci_completer/cmd/context_delete.go
+++ completers/circleci_completer/cmd/context_delete.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/completers/circleci_completer/cmd/action"
 	"github.com/spf13/cobra"
 )
 
@@ -15,4 +16,9 @@ func init() {
 	carapace.Gen(context_deleteCmd).Standalone()
 	context_deleteCmd.Flags().BoolP("force", "f", false, "Delete the context without asking for confirmation.")
 	contextCmd.AddCommand(context_deleteCmd)
+
+	// TODO org/context
+	carapace.Gen(context_deleteCmd).PositionalCompletion(
+		action.ActionVcsTypes(),
+	)
 }
--- completers/circleci_completer/cmd/context_list.go
+++ completers/circleci_completer/cmd/context_list.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/completers/circleci_completer/cmd/action"
 	"github.com/spf13/cobra"
 )
 
@@ -14,4 +15,9 @@ var context_listCmd = &cobra.Command{
 func init() {
 	carapace.Gen(context_listCmd).Standalone()
 	contextCmd.AddCommand(context_listCmd)
+
+	// TODO org-name
+	carapace.Gen(context_listCmd).PositionalCompletion(
+		action.ActionVcsTypes(),
+	)
 }
